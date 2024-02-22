b135e9be672d70536df6d0b8033f7464a4228758
timsparrie <timsparreboom@hotmail.com>
2024-02-22 13:29:24 +0100

    Scheduleview aanpasssing goed tableview

--- src/guiApplication/scheduleView/ScheduleView.java
+++ src/guiApplication/scheduleView/ScheduleView.java
@@ -1,10 +1,11 @@
 package guiApplication.scheduleView;
 
 
+import data.Journey;
 import data.Schedule;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class ScheduleView {
 
@@ -15,6 +16,15 @@ public class ScheduleView {
     }
 
     public Node getNode() {
-        return new Pane(new Label("Lorem ipsum")); //todo change view to right content
+        TableView<Journey> table = new TableView<Journey>();
+        TableColumn<Journey, String> peron = new TableColumn<Journey, String> ("Platform");
+        TableColumn<Journey, String> wagons = new TableColumn<Journey, String> ("Wagons(Capaciteit)");
+        TableColumn<Journey, String> train = new TableColumn<Journey, String> ("Trein");
+        TableColumn<Journey, String> arrival = new TableColumn<Journey, String> ("Aankomst");
+        TableColumn<Journey, String> departure = new TableColumn<Journey, String> ("Vertrek");
+
+        table.getColumns().addAll(peron, train, wagons, arrival, departure);
+
+        return table; //todo change view to right content
     }
 }
