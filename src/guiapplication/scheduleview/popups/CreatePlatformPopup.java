f5bef9b7ee2b18922c88a1d59a646f1726f742e3
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2024-02-23 22:29:06 +0100

    toegevoegd knoppen aan createplatformpopup

--- src/guiapplication/scheduleview/popups/CreatePlatformPopup.java
+++ src/guiapplication/scheduleview/popups/CreatePlatformPopup.java
@@ -1,6 +1,7 @@
 package guiapplication.scheduleview.popups;
 
 import data.Schedule;
+import data.ScheduleBuilder;
 import guiapplication.PopupView;
 import guiapplication.ReturnableView;
 import javafx.geometry.Insets;
@@ -14,26 +15,30 @@ import javafx.scene.layout.VBox;
 
 public class CreatePlatformPopup extends PopupView {
 
-    private Schedule schedule;
+    private ScheduleBuilder scheduleBuilder;
 
     public CreatePlatformPopup(ReturnableView mainView, Schedule schedule) {
         super(mainView);
-        this.schedule = schedule;
+        this.scheduleBuilder = new ScheduleBuilder(schedule);
     }
 
     @Override
     public Node getNode() {
         BorderPane pane = new BorderPane();
 
+        Label infoLabel = new Label("Voer platform nummer in:");
+        TextField inputField = new TextField();
+        VBox inputBox = new VBox(infoLabel, inputField);
+
         Button cancelButton = new Button("Annuleer");
         cancelButton.setOnAction(e -> super.callMainView());
         Button saveButton = new Button("Voeg toe");
-        FlowPane buttonBar = new FlowPane(cancelButton,saveButton);
+        saveButton.setOnAction(e -> {
+            this.scheduleBuilder.createPlatform(Integer.parseInt(inputField.getText()));
+            inputField.clear();
+        });
+        FlowPane buttonBar = new FlowPane(cancelButton, saveButton);
         buttonBar.setPadding(new Insets(10));
-
-        Label testLabel = new Label("Lorem Ipsum");
-        TextField testTextField = new TextField();
-        VBox inputBox = new VBox(testLabel, testTextField);
 
         pane.setCenter(inputBox);
         pane.setBottom(buttonBar);
