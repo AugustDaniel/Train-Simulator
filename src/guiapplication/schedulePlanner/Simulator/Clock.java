7bc374eb755554496509e7aefd3b1dcd1d5cdef8
davidVeer <151018675+davidVeer@users.noreply.github.com>
2024-03-16 13:15:33 +0100

    making sure clock is no longer unversioned

---
+++ src/guiapplication/schedulePlanner/Simulator/Clock.java
@@ -0,0 +1,55 @@
+package guiapplication.schedulePlanner.Simulator;
+
+import data.Journey;
+import data.Schedule;
+import util.TimeFormatter;
+
+import java.sql.Time;
+import java.time.LocalTime;
+
+
+public class Clock {
+    private double timeSpeed;
+    private Schedule schedule;
+    private double timePassed;
+    private int currentTimeInt;
+    private LocalTime currentTime;
+
+    public Clock(Schedule schedule, double timeSpeed) {
+        this.schedule = schedule;
+        this.timeSpeed = timeSpeed;
+        this.currentTime = TimeFormatter.intToLocalTime(currentTimeInt);
+        timePassed = 0;
+    }
+
+    public void update(double deltaTime) {
+        this.timePassed += deltaTime;
+        if (timePassed > this.timeSpeed){
+            currentTimeInt++;
+            if (currentTimeInt %100 > 59){
+                currentTimeInt -= 60;
+                currentTimeInt += 100;
+            }
+            if (currentTimeInt > 2359){
+                currentTimeInt -= 2400;
+            }
+            timePassed-=timeSpeed;
+            currentTime = TimeFormatter.intToLocalTime(currentTimeInt);
+        }
+
+        for (Journey journey : this.schedule.getJourneyList()) {
+            if (currentTime.equals(journey.getArrivalTime())){
+                System.out.println(currentTimeInt);
+                System.out.println("train number " + journey.getTrain() + " should now arrive at platform " + journey.getPlatform());
+            }
+        }
+    }
+
+    public void setTimeSpeed(double timeSpeed) {
+        this.timeSpeed = timeSpeed;
+    }
+
+    public LocalTime getCurrentTimeInt() {
+        return currentTime;
+    }
+}
