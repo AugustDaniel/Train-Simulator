dab03dbd8cccb937ba629d793342251c30d4d050
Zehond <fsas.7seal@gmail.com>
2024-03-20 10:50:21 +0100

    more performance fixes
    together with Jayson

--- src/guiapplication/schedulePlanner/Simulator/pathfinding/CollisionLayer.java
+++ src/guiapplication/schedulePlanner/Simulator/pathfinding/CollisionLayer.java
@@ -18,8 +18,8 @@ public class CollisionLayer {
 
         this.layerHeight = object.getInt("height");
         this.layerWidth = object.getInt("width");
-
         graph = new Graph(layerHeight, layerWidth);
+        Node[][] nodes = graph.getNodes();
         int index = 0;
 
         this.nodePositions = new Node[this.layerHeight][this.layerWidth];
@@ -30,42 +30,74 @@ public class CollisionLayer {
                     index++;
                     continue;
                 }
-
-                nodePositions[y][x] = new Node(new Point2D.Double(x * 32, y * 32));
-                index++;
-            }
-        }
-
-        for (int y = 0; y < this.layerHeight; y++) {
-            for (int x = 0; x < this.layerWidth; x++) {
-                Node currentNode = nodePositions[y][x];
+                Node currentNode = new Node(new Point2D.Double(x * 32, y * 32));
 
                 if (!(x - 1 < 0)) {
-                    addNodeAsAdjacent(currentNode, nodePositions[y][x - 1]);
+                    addNodeAsAdjacent(currentNode, nodes[y][x - 1]);
                 }
 
                 if (!(x + 1 > this.layerWidth - 1)) {
-                    addNodeAsAdjacent(currentNode, nodePositions[y][x + 1]);
+                    addNodeAsAdjacent(currentNode, nodes[y][x + 1]);
                 }
 
                 if (!(y - 1 < 0)) {
-                    addNodeAsAdjacent(currentNode, nodePositions[y - 1][x]);
+                    addNodeAsAdjacent(currentNode, nodes[y - 1][x]);
                 }
 
                 if (!(y + 1 > this.layerHeight - 1)) {
-                    addNodeAsAdjacent(currentNode, nodePositions[y + 1][x]);
+                    addNodeAsAdjacent(currentNode, nodes[y + 1][x]);
                 }
-
-                graph.addNode(y, x, currentNode);
+                graph.addNode(y,x,currentNode);
                 index++;
             }
         }
+//        for (int y = 0; y < this.layerHeight; y++) {
+//            for (int x = 0; x < this.layerWidth; x++) {
+//
+//                if (object.getJsonArray("data").getInt(index) == 1) {
+//                    index++;
+//                    continue;
+//                }
+//
+//                nodePositions[y][x] = new Node(new Point2D.Double(x * 32, y * 32));
+//                index++;
+//            }
+//        }
+
+//        for (int y = 0; y < this.layerHeight; y++) {
+//            for (int x = 0; x < this.layerWidth; x++) {
+//                Node currentNode = nodePositions[y][x];
+//
+//                if (!(x - 1 < 0)) {
+//                    addNodeAsAdjacent(currentNode, nodePositions[y][x - 1]);
+//                }
+//
+//                if (!(x + 1 > this.layerWidth - 1)) {
+//                    addNodeAsAdjacent(currentNode, nodePositions[y][x + 1]);
+//                }
+//
+//                if (!(y - 1 < 0)) {
+//                    addNodeAsAdjacent(currentNode, nodePositions[y - 1][x]);
+//                }
+//
+//                if (!(y + 1 > this.layerHeight - 1)) {
+//                    addNodeAsAdjacent(currentNode, nodePositions[y + 1][x]);
+//                }
+//
+//                graph.addNode(y, x, currentNode);
+//                index++;
+//            }
+//        }
         GraphTargetDB.getInstance().setGraphs(graph);
     }
 
     public void addNodeAsAdjacent(Node currentNode, Node toAdd) {
         if (toAdd != null && currentNode != null) {
             currentNode.addAdjacentNode(toAdd);
+            if (!toAdd.getAdjacentNodes().contains(currentNode)){
+                toAdd.addAdjacentNode(currentNode);
+            }
         }
+
     }
 }
