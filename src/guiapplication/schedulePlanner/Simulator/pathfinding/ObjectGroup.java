21a383275e7103b51c18536cfcc409d61cd296c8
Zehond <fsas.7seal@gmail.com>
2024-03-20 10:31:02 +0100

    performance fix
    together with Jayson

--- src/guiapplication/schedulePlanner/Simulator/pathfinding/ObjectGroup.java
+++ src/guiapplication/schedulePlanner/Simulator/pathfinding/ObjectGroup.java
@@ -12,15 +12,17 @@ public class ObjectGroup {
     public ObjectGroup(JsonObject jsonObject){
         JsonArray objects = jsonObject.getJsonArray("objects");
         Graph graph = GraphTargetDB.getInstance().getGraph();
+        Node[][] graphNodes = graph.getNodes();
+       GraphTargetDB instanceGraph = GraphTargetDB.getInstance();
         for (int i = 0; i < objects.size(); i++) {
             int xObject = objects.getJsonObject(i).getInt("x")/ 32;
             int yObject = objects.getJsonObject(i).getInt("y")/ 32;
-            for (int y = 0; y < objects.getJsonObject(i).getInt("height") /32; y++) {
-                for (int x = 0; x < objects.getJsonObject(i).getInt("width") /32 ; x++) {
-                    Node nodeToAdd =graph.getNodes()[yObject + y][xObject + x ];
+            for (int y = 0; y < objects.getJsonObject(i).getInt("height") /32; y+=2) {
+                for (int x = 0; x < objects.getJsonObject(i).getInt("width") /32 ; x+=2) {
+                    Node nodeToAdd = graphNodes[yObject + y][xObject + x ];
 
                     if(nodeToAdd != null) {
-                        GraphTargetDB.getInstance().addTarget(new Target(nodeToAdd));
+                        instanceGraph.addTarget(new Target(nodeToAdd));
                     }
                 }
             }
