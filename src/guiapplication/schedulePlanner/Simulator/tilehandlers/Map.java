f8ccf87adadea2e640ab5879d11fea08a5a08d5c
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2024-03-20 17:44:34 +0100

    removed turned object var into local var

--- src/guiapplication/schedulePlanner/Simulator/tilehandlers/Map.java
+++ src/guiapplication/schedulePlanner/Simulator/tilehandlers/Map.java
@@ -12,14 +12,12 @@ import java.awt.*;
 public class Map implements TileHandler {
     private TileSet tileSet;
     private TileMap tileMap;
-    private JsonObject root;
 
     public Map(String fileName) {
-
         JsonReader reader = Json.createReader(getClass().getResourceAsStream(fileName));
-        this.root = reader.readObject();
-        this.tileSet = new TileSet(this.root.getJsonArray("tilesets"));
-        this.tileMap = new TileMap(this.root.getJsonArray("layers"), this.tileSet);
+        JsonObject root = reader.readObject();
+        this.tileSet = new TileSet(root.getJsonArray("tilesets"));
+        this.tileMap = new TileMap(root.getJsonArray("layers"), this.tileSet);
     }
 
     @Override
