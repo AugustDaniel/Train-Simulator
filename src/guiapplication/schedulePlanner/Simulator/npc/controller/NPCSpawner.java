32bbc93b4501d2c074b8e74189b1b6f0dc7977be
Zehond <fsas.7seal@gmail.com>
2024-04-01 18:30:11 +0200

    removed line of code

--- src/guiapplication/schedulePlanner/Simulator/npc/controller/NPCSpawner.java
+++ src/guiapplication/schedulePlanner/Simulator/npc/controller/NPCSpawner.java
@@ -52,7 +52,6 @@ public class NPCSpawner implements util.Observer{
             if (Math.random() > 0.9) {
                 traveler.setStatus(Traveler.Status.SHOPPING);
                 traveler.setTarget(new Target(PathFinding.getRandomShoppingTarget()));
-                traveler.setTarget(new Target(PathFinding.getRandomSpawnPoint()));
             }
 
             npcs.add(traveler);
